3350cb28de5a5c891ef372f776f9e7d28ebcafd2
JhonCuellar <jhon10_119@hotmail.com>
2021-02-22 18:30:57 +0000

    Jhon Cuellar

--- control_de_flujo.py
+++ control_de_flujo.py
@@ -104,10 +104,16 @@ Nota: Un número primo es un número entero que no se puede calcular multiplican
 otros números enteros.
 """
 
-lista3 = list(range(37,300,1))
+lista3 = list(range(37,301,1))
 primos = []
+a = 0
 for el in lista3:
-  if el % 2 == 0
+  for n in range(2, el):
+    if el % n == 0:
+      a = el
+      break
+  if(a  != el):
+    primos.append(el)
 
 """Guardar en `fibonacci` una lista con los primeros 60 términos de la serie de 
 Fibonacci.
@@ -117,10 +123,15 @@ del segundo cada uno se calcula sumando los dos anteriores términos de la serie
 [0, 1, 1, 2, 3, 5, 8, ...]
 
 """
-
-
-
-
+fibonacci = [0]
+f = 0
+f1 = 1
+f2 = 0
+for i in range(1, 60):
+  f1 = f2 + f1
+  f2 = f
+  f = f1
+  fibonacci.append(f1)
 
 """Guardar en `factorial` el factorial de 30
 El factorial (símbolo:!) Significa multiplicar todos los números enteros desde
@@ -131,8 +142,10 @@ Por ejemplo, el factorial de 5 se calcula así:
 5! = 5 × 4 × 3 × 2 × 1 = 120
 """
 
+factorial = 1
 
-
+for f in range(30,0 ,-1):
+  factorial = factorial * f
 
 
 """Guarde en lista `pares` los elementos de la siguiente lista que esten 
@@ -141,25 +154,33 @@ presentes en posiciones pares, pero solo hasta la posición 80.
 
 lista3 = [941, 149, 672, 208, 99, 562, 749, 947, 251, 750, 889, 596, 836, 742, 512, 19, 674, 142, 272, 773, 859, 598, 898, 930, 119, 107, 798, 447, 348, 402, 33, 678, 460, 144, 168, 290, 929, 254, 233, 563, 48, 249, 890, 871, 484, 265, 831, 694, 366, 499, 271, 123, 870, 986, 449, 894, 347, 346, 519, 969, 242, 57, 985, 250, 490, 93, 999, 373, 355, 466, 416, 937, 214, 707, 834, 126, 698, 268, 217, 406, 334, 285, 429, 130, 393, 396, 936, 572, 688, 765, 404, 970, 159, 98, 545, 412, 629, 361, 70, 602]
 
+pares = []
 
-
-
+for p in range(0,81):
+  if(p % 2 == 0):
+    pares.append(lista3[p])
 
 
 """Guarde en lista `cubos` el cubo (potencia elevada a la 3) de los números del 
 1 al 100. 
 """
+cubos = []
 
-
+for c in range(1,101):
+  cubos.append(pow(c,3))
 
 
 
 """Encuentre la suma de la serie 2 +22 + 222 + 2222 + .. hasta sumar 10 términos 
 y guardar resultado en variable `suma_2s` 
 """
-
-
-
+suma_2s = 2
+s = 1
+ss = "2"
+while s < 10:
+  ss = ss + "2"
+  suma_2s = suma_2s + int(ss)
+  s += 1
 
 
 """Guardar en un string llamado `patron` el siguiente patrón llegando a una 
@@ -183,6 +204,6 @@ cantidad máxima de asteriscos de 30.
 *
 """
 
-
+patron = ""
 
 
